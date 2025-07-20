8f834b64c7a3a017a681d3d21960f7e44c702b22
p-mn19 <poornimamen2019@gmail.com>
2025-07-20 18:49:43 +0530

    commit

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import { SidebarComponent } from '@syncfusion/ej2-react-navigations';
+import { NavItems } from 'components';
 
 const AdminLayout = () => {
     return(
@@ -8,7 +9,7 @@ const AdminLayout = () => {
             MobileSidebar
             <aside className='w-full max-w-[270px] hidden lg:block'>
                 <SidebarComponent width={270} enableGestures={false} >
-
+                <NavItems />
                 </SidebarComponent>
             </aside>
             <aside className='children'>
