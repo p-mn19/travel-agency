17ba09a41f64d251d496e2494e05d36d0063ab56
p-mn19 <poornimamen2019@gmail.com>
2025-10-29 13:47:19 +0530

    commit

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,40 +1,44 @@
-//@ts-nocheck
-
+import React, { useRef } from "react";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import NavItems from "./NavItems";
 
-const MobileSidebar = () =>{
-    let sidebar:SidebarComponent;
-    const toggleSidebar =() =>{
-        sidebar.toggle()
-    }
-    return(
-        <div className="mobile-sidebar wrapper">
-            <header>
-                <Link to="/">
-                <img
-                    src="/assets/icons/logo.svg"
-                    alt="Logo"
-                    className="size-[30px]"
-                />
-                <h1>Tourister</h1>
-                </Link>
-                <button onClick={toggleSidebar}></button>
-                <img src="/assets/icons/menu.svg" alt="menu" className="size-7"/>
-            </header>
-            <SidebarComponent 
-            width={270}
-            ref={(Sidebar) => sidebar=Sidebar}
-            created={()=>sidebar.hide()}
-            closeOnDocumentClick={true}
-            showBackdrop={true}
-            type="over"
-            >
-                <NavItems handleClick={toggleSidebar}/>
-            </SidebarComponent>
-        </div>
-    )
-}
+const MobileSidebar = () => {
+  const sidebarRef = useRef<SidebarComponent | null>(null);
+
+  const toggleSidebar = () => {
+    sidebarRef.current?.toggle();
+  };
+
+  return (
+    <div className="mobile-sidebar wrapper">
+      <header className="flex items-center justify-between p-4">
+        <Link to="/" className="flex items-center gap-2">
+          <img
+            src="/assets/icons/logo.svg"
+            alt="Logo"
+            className="size-[30px]"
+          />
+          <h1 className="text-lg font-semibold">Tourister</h1>
+        </Link>
+
+        <button onClick={toggleSidebar} className="cursor-pointer">
+          <img src="/assets/icons/menu.svg" alt="menu" className="size-7" />
+        </button>
+      </header>
+
+      <SidebarComponent
+        width={270}
+        ref={sidebarRef}
+        created={() => sidebarRef.current?.hide()}
+        closeOnDocumentClick={true}
+        showBackdrop={true}
+        type="Over"
+      >
+        <NavItems handleClick={toggleSidebar} />
+      </SidebarComponent>
+    </div>
+  );
+};
 
 export default MobileSidebar;
